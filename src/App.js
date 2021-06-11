078a77f1e9b65fd05e6575d7d33aefa334a31db8
TadjievCodes <muhammad2008.08@mail.ru>
2021-06-10 20:10:43 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import Todolist from './Todo/TodoList';
+
+
+function App() {
+
+const todos = [ 
+  {id: 1, completed: false, title: "Buy bread"},
+  {id: 2, completed: false, title: "Buy butter"},
+  {id: 3, completed: false, title: "Buy milk"}
+
+]
+
+ function toggleTodo(id) {
+      
+      
+
+    console.log('todo id',id);
+ } 
+
+
+  return (
+  <div className="wrapper"> 
+    <h1>React TodoList</h1>
+      <Todolist todos={todos} onToggle={toggleTodo}/>
+  </div>
+  
+  );
+}
+
+export default App;
