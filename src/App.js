7293f4460a8b0c28aa1822d7edaad859437693d8
TadjievCodes <muhammad2008.08@mail.ru>
2021-06-12 23:05:28 -0400

    added a loader spinning

--- src/App.js
+++ src/App.js
@@ -3,25 +3,30 @@ import logo from './logo.svg';
 import Todolist from './Todo/TodoList';
 import Context from './context'
 import AddTodo from './Todo/AddTodo'
-
+import Loader from './Loader'
 
 
 function App() {
 
-const [todos, setTodos] = React.useState([
-  
-  {id: 1, completed: false, title: "Buy bread"},
-  {id: 2, completed: true, title: "Buy butter"},
-  {id: 3, completed: false, title: "Buy milk"}
+const [todos, setTodos] = React.useState([])
+const [loading, setLoading] = React.useState(true)
 
-])
+  /*
+  {id: 1, completed: false, title: "Buy bread"}
+  {id: 2, completed: true, title: "Buy butter"}
+  {id: 3, completed: false, title: "Buy milk"}
+*/
+
 
 
 useEffect(  () => {
  fetch('https://jsonplaceholder.typicode.com/todos/?_limit=5')
   .then(response => response.json())
   .then(todos => {
-      setTodos(todos)
+      setTimeout( () => {  
+        setTodos(todos)
+        setLoading(false)
+      }, 2000)
   })
 
 },  [])
@@ -59,9 +64,11 @@ function addTodo(title) {
     <div className="wrapper"> 
       <h1>React TodoList</h1>
         <AddTodo onCreate={addTodo} />
+
+        {loading && <Loader />}
         {todos.length ?  (
            <Todolist todos={todos} onToggle={toggleTodo} /> 
-           ) : (   
+           ) :   loading ? null  :  (      
             <p>No Todolist records!</p>   
            )}
      
